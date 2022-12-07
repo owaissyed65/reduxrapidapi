6b6246b0a67e4d706557c5228ec7bccf43960c0b
owaissyed65 <owaissyed65@gmail.com>
2022-12-08 00:11:57 +0500

    initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import { option } from './option';
 
-function App() {
+const App = () => {
+
+  
+
+  const [value, setValue] = useState("")
+  console.log(value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cont'>
+      <input type="text" name="" id="" />
+      <Dropdown options={option} onChange={(e) => { setValue(e.value); }} placeholder="Select an Language" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/option.js
@@ -0,0 +1,11 @@
+export const option = [
+    { value: 'ur', label: 'Urdu' },
+    { value: 'af', label: 'Afrikaans' },
+    { value: 'sq', label: 'Albanian' },
+    { value: 'ar', label: 'Arabic' },
+    { value: 'hy', label: 'Armenian' },
+    { value: 'az', label: 'Azerbaijani' },
+    { value: 'az', label: 'Basque' },
+    { value: 'be', label: 'Belarusian' },
+    { value: 'bn', label: 'Bengali' },
+]
