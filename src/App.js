70e2fda69e2021773b8530f867b22360503c7d7c
owaissyed65 <owaissyed65@gmail.com>
2022-12-09 23:04:40 +0500

    Some css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { useState } from 'react'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-import { option } from './option';
+import { transChoose, translateAfter } from './option';
 
 const App = () => {
 
-  
 
-  const [value, setValue] = useState("")
-  console.log(value)
+
+  const [translateDrop, setTranslateDrop] = useState({});
+  const [transAfter, setTranAfter] = useState({});
+  const [transAfterValue, setTranAftervalue] = useState({ trnstr: '' });
+  const [transBeforeValue, setTranBeforeValue] = useState({ trnstr: '' });
+  console.log(translateDrop)
   return (
     <div className='cont'>
-      <input type="text" name="" id="" />
-      <Dropdown options={option} onChange={(e) => { setValue(e.value); }} placeholder="Select an Language" />
+      <h2>Translate {Object.keys(translateDrop).length !== 0 ? translateDrop.label : "English"} language to {Object.keys(transAfter).length !== 0 ? transAfter.label : "Urdu"}</h2>
+      <div className='cont-child'>
+        <textarea name="trnstr" cols="30" rows="5" placeholder='Enter Text' onChange={(e) => {
+          setTranAftervalue({
+            ...transAfterValue, [e.target.name]: e.target.value
+          })
+        }}></textarea>
+        <Dropdown className='drop' options={transChoose} value={transChoose[9].label} onChange={(e) => { setTranslateDrop({ ...e }); }} placeholder="Select an Language" />
+      </div>
+      <div className='cont-child'>
+        <textarea name="" id="" cols="30" rows="5" readOnly placeholder='Translation'></textarea>
+        <Dropdown className='drop' options={translateAfter} onChange={(e) => {
+          setTranBeforeValue({
+            ...transBeforeValue, [e.target.name]: e.target.value
+          })
+        }} placeholder="Select an Language" />
+      </div>
+      <div className='cont-child-btn'><button >Translate</button></div>
     </div>
+
   )
 }
 
--- src/option.js
+++ src/option.js
@@ -1,4 +1,16 @@
-export const option = [
+export const transChoose = [
+    { value: 'ur', label: 'Urdu' },
+    { value: 'af', label: 'Afrikaans' },
+    { value: 'sq', label: 'Albanian' },
+    { value: 'ar', label: 'Arabic' },
+    { value: 'hy', label: 'Armenian' },
+    { value: 'az', label: 'Azerbaijani' },
+    { value: 'az', label: 'Basque' },
+    { value: 'be', label: 'Belarusian' },
+    { value: 'bn', label: 'Bengali' },
+    { value: 'en', label: 'English' },
+]
+export const translateAfter = [
     { value: 'ur', label: 'Urdu' },
     { value: 'af', label: 'Afrikaans' },
     { value: 'sq', label: 'Albanian' },
